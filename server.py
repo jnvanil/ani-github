2d2438cd7f76de313cdc406f95e3e8a5575b6131
Anil Kumar Singh <35450239+jnvanil@users.noreply.github.com>
2019-03-31 23:06:47 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,29 @@
+import socket
+import sys
+import time
+
+
+s = socket.socket()
+host = socket.gethostname()
+print("Server will start on :",host)
+port = 8080
+s.bind((host,port))
+print("")
+print("server done binding to host and port successful:")
+print("")
+print ("server is waiting for incoming connections")
+print("")
+s.listen(1)
+conn,addr = s.accept()
+print (addr,"has connected to the server and is now online ...")
+print("")
+while 1:
+    message = input(str(">>"))
+    message = message.encode()
+    conn.send(message)
+    print("message has been sent...")
+    print("")
+    incoming_message = conn.recv(1024)
+    incoming_message = incoming_message.decode()
+    print("client : ", incoming_message)
+    print("")
